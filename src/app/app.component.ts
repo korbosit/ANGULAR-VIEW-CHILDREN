65064822c004d23119d4edf8ae83e7aa98a8f5d0
Vlaimir Korba <krissky1986@gmail.com>
2024-02-14 17:52:23 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  fullName: string = '';
+  @ViewChildren('inputEl') inputElements: QueryList<ElementRef>;
+
+  ngOnInit() {
+    this.inputElements.forEach((el) => {
+      console.log(el.nativeElement.value);
+    });
+  }
+
+  show() {
+    let name = '';
+    this.inputElements.forEach((el) => {
+      name += el.nativeElement.value + ' ';
+    });
+    this.fullName = name.trim();
+  }
+}
